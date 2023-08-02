c909977cf369c1212fe312d00566957f9b259d15
Holiday <87312690+beegees303@users.noreply.github.com>
2023-08-02 11:10:18 +0900

    Create watermark.py

---
+++ watermark.py
@@ -0,0 +1,30 @@
+import random
+from PIL import Image, ImageDraw, ImageFont
+import os
+
+path = "./"
+file_list = os.listdir(path)
+file_list = [file for file in file_list if file.endswith(".jpg")]
+
+for i in file_list:
+
+    img = Image.open(i).convert("RGBA")
+    file_name=os.path.basename(i)
+    
+    txt = Image.new('RGBA', img.size, (255,255,255,0))
+ 
+    text = "noexpect.tistory.com"
+    font = ImageFont.truetype("arial.ttf",int(min(img.size)/10))
+
+    d = ImageDraw.Draw(txt)
+
+    width, height = img.size 
+    textwidth, textheight = d.textsize(text, font)
+    x=width/2-textwidth/2
+    y=height/2-textheight/2
+
+    d.text((x,y), text, fill=(255,255,255,150), font=font)
+
+    watermarked = Image.alpha_composite(img, txt)
+    
+    watermarked.save(file_name+'.png')
